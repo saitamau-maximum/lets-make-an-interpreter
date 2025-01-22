ac13b0bbf06946c6ee3aad71afb40f43889fd15e
Sor4chi <80559385+sor4chi@users.noreply.github.com>
2025-01-22 16:33:53 +0900

    feat: evaluate empty/number only

--- src/evaluator.test.ts
+++ src/evaluator.test.ts
@@ -4,6 +4,11 @@ import { parse } from "./parser";
 import { lex } from "./lexer";
 
 describe("evaluate", () => {
+  it("should evaluate empty program", () => {
+    const input = "";
+    expect(evaluate(parse(lex(input)))).toEqual(0);
+  });
+
   it("should evaluate integer literal", () => {
     const input = "1";
     expect(evaluate(parse(lex(input)))).toEqual(1);
--- src/evaluator.ts
+++ src/evaluator.ts
@@ -1,5 +1,34 @@
-import { Program } from "./parser";
+import { Expression, Program, Statement } from "./parser";
+
+type Env = Record<string, number>; // Variable name to value mapping (scope)
+
+const evaluateProgram = (program: Program, env: Env): number => {
+  let result = 0;
+  for (const statement of program.body) {
+    result = evaluateStatement(statement, env);
+  }
+  return result;
+};
+
+const evaluateStatement = (statement: Statement, env: Env): number => {
+  switch (statement.type) {
+    case "ExpressionStatement":
+      return evaluateExpression(statement.expression, env);
+    default:
+      throw new Error(`Unknown statement: ${statement}`);
+  }
+};
+
+const evaluateExpression = (expression: Expression, env: Env): number => {
+  switch (expression.type) {
+    case "IntegerLiteral":
+      return expression.value;
+    default:
+      throw new Error(`Unknown expression: ${expression}`);
+  }
+};
 
 export const evaluate = (program: Program): number => {
-  return 0;
+  const env: Env = {};
+  return evaluateProgram(program, env);
 };
